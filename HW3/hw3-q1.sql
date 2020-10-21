79da84fe93112def87a83126175b405d250bdf32
Eric Boris <the.eric.boris@gmail.com>
2020-10-20 21:59:33 -0700

    clean up hw3-q1

--- HW3/hw3-q1.sql
+++ HW3/hw3-q1.sql
@@ -1,9 +1,30 @@
-/* 
-The SQL query that once executed returns the expected result.
-    ● A comment that indicated the number of rows your query returns
-    ● A comment that indicates how long the query took, and
-    ● A comment that contains the first 20 rows of the result (if the result has fewer than 20 rows, output all of them).
-    ○ You can simply copy and paste the first rows into the comment. 
+/*
+The number of rows your query returns:
+	334
+How long the query took:
+	2s
+The first 20 rows of the result:
+	origin_city,dest_city,time
+	Aberdeen SD,Minneapolis MN,106
+	Abilene TX,Dallas/Fort Worth TX,111
+	Adak Island AK,Anchorage AK,471
+	Aguadilla PR,New York NY,368
+	Akron OH,Atlanta GA,408
+	Albany GA,Atlanta GA,243
+	Albany NY,Atlanta GA,390
+	Albuquerque NM,Houston TX,492
+	Alexandria LA,Atlanta GA,391
+	Allentown/Bethlehem/Easton PA,Atlanta GA,456
+	Alpena MI,Detroit MI,80
+	Amarillo TX,Houston TX,390
+	Anchorage AK,Barrow AK,490
+	Appleton WI,Atlanta GA,405
+	Arcata/Eureka CA,San Francisco CA,476
+	Asheville NC,Chicago IL,279
+	Ashland WV,Cincinnati OH,84
+	Aspen CO,Los Angeles CA,304
+	Atlanta GA,Honolulu HI,649
+	Atlantic City NJ,Fort Lauderdale FL,212
 */
 
 SELECT DISTINCT 
@@ -19,3 +40,4 @@ SELECT DISTINCT
    AND f1.actual_time = f2.max_time
  ORDER BY f1.origin_city ASC, 
        f1.dest_city ASC;
+
