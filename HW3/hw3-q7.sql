49e858950b5361e1d6c8103dbc63c3a64f558abe
Eric Boris <the.eric.boris@gmail.com>
2020-10-21 23:23:41 -0700

    finish q7

---
+++ HW3/hw3-q7.sql
@@ -0,0 +1,27 @@
+/*
+7. List the names of carriers that operate flights from Seattle to San Francisco, CA. Return each carrier's name only once. Do not use a nested query to answer this question.
+
+Name the output column ​carrier​. Order the output ascending by carrier.
+[Output relation cardinality: 4]
+
+The number of rows the query returns:
+	4
+How long the query took:
+	2s
+The first 20 rows of the result:
+	carrier
+	Alaska Airlines Inc.
+	SkyWest Airlines Inc.
+	United Air Lines Inc.
+	Virgin America
+*/
+
+SELECT DISTINCT
+       c.name AS carrier
+  FROM carriers AS c 
+  JOIN flights AS f 
+    ON f.carrier_id = c.cid
+ WHERE f.origin_city = 'Seattle WA' 
+   AND f.dest_city = 'San Francisco CA'
+ GROUP BY c.name
+ ORDER BY c.name;
