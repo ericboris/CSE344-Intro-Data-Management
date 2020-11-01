6ec89754354e37b0519c698027efac49105dc356
Eric Boris <the.eric.boris@gmail.com>
2020-10-31 23:28:09 -0700

    add hw 4 p2

---
+++ HW4/p2.sql
@@ -0,0 +1,53 @@
+CREATE TABLE InsuranceCo (
+    name varchar(50),
+    phone int,
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE InsurancePlan (
+    maxLiability float,
+    FOREIGN KEY (insurer) REFERENCES InsuranceCo (name),
+    FOREIGN KEY (licensePlate) REFERENCES Vehicle (licensePlate)  
+);
+
+CREATE TABLE Vehicle (
+    licensePlate varchar(7),
+    year int,
+    PRIMARY KEY (licensePlate)
+    FOREIGN KEY (ownedByy) REFERENCES Person (ssn)
+    FOREIGN KEY (insuredBy) REFERENCES InsuranceCo (name)
+);
+
+CREATE TABLE Person (
+    ssn int,
+    name varchar(50)
+    PRIMARY KEY (ssn)
+);
+
+CREATE TABLE Driver (
+    driverId int,
+    PRIMARY KEY (driverId)
+    FOREIGN KEY (drivenBy) REFERENCES NonProfessionalDriver (ssn)
+);
+
+CREATE TABLE Car (
+    make varchar(50)
+    FOREIGN KEY (licensePlate) REFERENCES Vehicle (licensePlate)
+);
+
+CREATE TABLE Truck (
+    capacity int
+    FOREIGN KEY (licensePlate) REFERENCES Vehicle (licensePlate)
+    FOREIGN KEY (operatedBy) REFERENCES ProfessionalDriver (driverId)
+);
+
+CREATE TABLE NonProfessionalDriver (
+    FOREIGN KEY (driverId) REFERENCES Driver (driverId)
+);
+
+CREATE TABLE ProfessionalDriver (
+    medicalHistory varchar(50) 
+    FOREIGN KEY (driverId) REFERENCES Driver (driverId)
+);
+
+
